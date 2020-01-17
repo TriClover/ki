e6045bfc098833092fe9b1f9beccc39b7a9cab0c
Anthony Heathcoat <heathcoata@midwestloanservices.com>
2020-01-17 14:05:49 -0500

    Make header sending optional in the init

--- src/Ki.php
+++ src/Ki.php
@@ -4,7 +4,7 @@ namespace mls\ki;
 class Ki
 {
 	public static $siteName = NULL;
-	public static function init(string $siteName)
+	public static function init(string $siteName, bool $sendHeaders = true)
 	{
 		Ki::$siteName = $siteName;
 		\ini_set("default_charset", "UTF-8");
@@ -12,7 +12,10 @@ class Ki
 		\mb_http_output('UTF-8');
 		\iconv_set_encoding("internal_encoding", "UTF-8");
 		\iconv_set_encoding("output_encoding", "UTF-8");
-		header('Content-Type: text/html; charset=UTF-8');
+		if($sendHeaders)
+		{
+			header('Content-Type: text/html; charset=UTF-8');
+		}
 		date_default_timezone_set(Config::get()['general']['timezone']);
 	}
 }
